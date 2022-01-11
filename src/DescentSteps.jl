8f1063269a272035b8f81589be032025170fd386
pat-alt <altmeyerpat@gmail.com>
2022-01-11 10:02:10 +0100

    attempt at multi dispatch

--- src/DescentSteps.jl
+++
@@ -1,9 +0,0 @@
-module DescentSteps
-
-function gradient_descent
-end
-
-function choose_most_salient
-end
-
-end
---
+++ src/generators.jl
@@ -0,0 +1,82 @@
+# generators.jl
+#
+# Core package functionality that implements algorithmic recourse.
+
+# --------------- Base type for generator:
+abstract type Generator end
+
+# -------- Main method:
+function generate_recourse(generator::Generator, x̅::Vector{x}, 𝓜::Function, target::Float64; T=1000, 𝓘=[])
+    
+    # Setup and allocate memory:
+    x̲ = copy(x̅) # start from factual
+    D = length(x̲)
+    path = reshape(x̲, 1, length(x̲)) # storing the path
+
+    # Initialize:
+    t = 1 # counter
+    converged = convergence(generator, x̲, 𝓜, target, x̅)
+
+    # Search:
+    while !converged && t < T 
+        x̲ = step(generator, x̲, 𝓜, target, x̅, 𝓘)
+        t += 1 # update number of times feature is changed
+        converged = convergence(generator, x̲, 𝓜, target, x̅) # check if converged
+        path = vcat(path, reshape(x̲, 1, D))
+    end
+
+    # Output:
+    y̲ = 𝓜(x̲)
+    recourse = Recourse(x̲, y̲, path, generator, immutable_, x̅, 𝓜, target) 
+    
+    return recourse
+    
+end
+
+# --------------- Specific generators:
+
+# -------- Wachter et al (2018): 
+struct GenericGenerator <: Generator
+    λ::Float64 # strength of penalty
+    ϵ::Float64 # step size
+    τ::Float64 # tolerance for convergence
+end
+
+ℓ(generator::GenericGenerator, x, 𝓜, t) = - (t * log(𝛔(𝓜(x))) + (1-t) * log(1-𝛔(𝓜(x))))
+complexity(generator::GenericGenerator, x̅, x̲) = norm(x̅,x̲)
+objective(generator::GenericGenerator, x̲, 𝓜, t, x̅) = ℓ(generator, x̲, 𝓜, t) + generator.λ * complex(generator, x̅, x̲) 
+∇(generator::GenericGenerator, x̲, 𝓜, t, x̅) = gradient(() -> objective(generator, x̲, 𝓜, t, x̅), params(x̲))
+
+function step(generator::GenericGenerator, x̲, 𝓜, t, x̅, 𝓘) 
+    𝐠ₜ = ∇(generator, x̲, 𝓜, t, x̅)
+    𝐠ₜ[𝓘] .= 0 # set gradient of immutable features to zero
+    return x̲ - (generator.ϵ .* 𝐠ₜ)
+end
+
+function convergence(generator::GenericGenerator, x̅, 𝓜, target, x̲)
+    all(∇(generator, x̲, 𝓜, t, x̅) .< generator.τ)
+end
+
+# -------- Schut et al (2021):
+struct GreedyGenerator <: Generator
+    Γ::Float64 # desired level of confidence 
+    δ::Float64 # perturbation size
+    n::Int64 # maximum number of times any feature can be changed
+end
+
+ℓ(generator::GreedyGenerator, x, 𝓜, t) = - (t * log(𝛔(𝓜(x))) + (1-t) * log(1-𝛔(𝓜(x))))
+objective(generator::GreedyGenerator, x̲, 𝓜, t) = ℓ(generator, x̲, 𝓜, t) 
+∇(generator::GreedyGenerator, x̲, 𝓜, t) = gradient(() -> objective(generator, x̲, 𝓜, t), params(x̲))
+
+function step(generator::GreedyGenerator, x̲, 𝓜, t, x̅, 𝓘) 
+    𝐠ₜ = ∇(generator, x̲, 𝓜, t)
+    𝐠ₜ[𝓘] .= 0 # set gradient of immutable features to zero
+    iₜ = argmax(abs.(𝐠ₜ)) # choose most salient feature
+    x̲[iₜ] -= generator.δ * sign(𝐠ₜ[iₜ]) # counterfactual update
+    return x̲
+end
+
+function convergence(generator::GreedyGenerator, x̲, 𝓜, t, x̅)
+    𝓜.confidence(x̲) .> generator.Γ
+end
+
