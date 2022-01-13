6d110552de556d36300880dfd081d6c5431a6621
pat-alt <altmeyerpat@gmail.com>
2022-01-13 09:51:18 +0100

    fixed small errors after it failed to compile

--- src/AlgorithmicRecourse.jl
+++ src/AlgorithmicRecourse.jl
@@ -7,12 +7,12 @@ using LinearAlgebra
 include("models/Models.jl")
 using .Models
 
+include("losses/Losses.jl")
+using .Losses
+
 include("generators/Generators.jl")
 using .Generators
 export Generator, GenericGenerator
-
-include("losses/Losses.jl")
-using .Losses
 
 include("core.jl")
 export generate_recourse
--- src/losses/functions.jl
+++ src/losses/functions.jl
@@ -1,9 +1,11 @@
+using Flux
+
 """
     hinge_loss(ŷ, y)
 
-Uses [Flux.hinge_loss](https://fluxml.ai/Flux.jl/stable/models/losses/#Flux.Losses.hinge_loss) to compute Hinge loss. If provided labels are in `\{0,1\}` they are mapped to `\{-1,1\}`. 
+Uses [Flux.Losses.hinge_loss](https://fluxml.ai/Flux.jl/stable/models/losses/#Flux.Losses.hinge_loss) to compute Hinge loss. If provided labels are in `{0,1}` they are mapped to `{-1,1}`. 
 """
 function logitbinarycrossentropy(ŷ, y)
     z(y) = ifelse(y==0,-1,1) # adapt in case labels are in {0,1}
-    return Flux.logitbinarycrossentropy(z(ŷ), z(y))
+    return Flux.Losses.logitbinarycrossentropy(z(ŷ), z(y))
 end
