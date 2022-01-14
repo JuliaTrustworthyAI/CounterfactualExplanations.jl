5d606f75b9ed56d24e7f5ec85f065cea1afbe5a5
pat-alt <altmeyerpat@gmail.com>
2022-01-14 09:24:28 +0100

    fixed another error in hinge function

--- src/losses/functions.jl
+++ src/losses/functions.jl
@@ -6,6 +6,6 @@ using Flux
 Uses [Flux.Losses.hinge_loss](https://fluxml.ai/Flux.jl/stable/models/losses/#Flux.Losses.hinge_loss) to compute Hinge loss. If provided labels are in `{0,1}` they are mapped to `{-1,1}`. 
 """
 function hinge_loss(ŷ, y)
-    h(y) = ifelse(y==0,-1,1) # adapt in case labels are in {0,1}
+    h(y) = ifelse(y==0,-1,y) # adapt in case labels are in {0,1}
     return Flux.Losses.hinge_loss(ŷ, h(y))
 end
