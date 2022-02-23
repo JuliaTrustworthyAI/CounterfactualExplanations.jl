bc5c49bd130eb69bf7728be6771365b7b032f9d8
pat-alt <altmeyerpat@gmail.com>
2022-02-23 19:25:25 +0100

    trained model for CIFAR data

--- docs/src/examples/CIFAR10.jl
+++ docs/src/examples/CIFAR10.jl
@@ -7,45 +7,68 @@ using Base.Iterators: partition
 using CUDA
 
 # Data
-train_x, train_y = CIFAR10.traindata(Float32)
-labels = onehotbatch(train_y, 0:9)
-N = 50000
-batch_size = 1000
-train = ([(train_x[:,:,:,i], labels[:,i]) for i in partition(1:(N-batch_size), batch_size)]) |> gpu
-valset = (N-batch_size+1):N
-valX = train_x[:,:,:,valset] |> gpu
-valY = labels[:, valset] |> gpu
+x, y = CIFAR10.traindata(Float32)
 
-# # CNN
-# m = Chain(
-#   Conv((5,5), 3=>16, relu),
-#   MaxPool((2,2)),
-#   Conv((5,5), 16=>8, relu),
-#   MaxPool((2,2)),
-#   x -> reshape(x, :, size(x, 4)),
-#   Dense(200, 120),
-#   Dense(120, 84),
-#   Dense(84, 10),
-#   softmax) |> gpu
+# Turn into binary problem (cats/dogs)
+class_names = CIFAR10.classnames()
+n_classes = length(class_names)
+c_cat = findall(class_names.=="cat") .- 1
+c_dog = findall(class_names.=="dog") .- 1
+cats_and_dogs = findall(y .∈ Ref(vcat(c_cat, c_dog)))
 
-# using Flux: crossentropy, Momentum
-# loss(x, y) = sum(crossentropy(m(x), y))
-# opt = Momentum(0.01)
-# accuracy(x, y) = mean(onecold(m(x), 0:9) .== onecold(y, 0:9))
+x = x[:,:,:,cats_and_dogs]
+y = y[cats_and_dogs]
+y = ifelse.(y.==c_cat,0,1)
 
-# # Training
-# epochs = 10
-# for epoch = 1:epochs
-#   for d in train
-#     gs = gradient(params(m)) do
-#       l = loss(d...)
-#     end
-#     update!(opt, params(m), gs)
-#   end
-#   @show accuracy(valX, valY)
-# end
+n = length(y)
+batch_size = Int(n/10)
+train_set = 1:(n-batch_size)
+val_set = (n-batch_size+1):n
+train_x = x[:,:,:,train_set]
+train_y = y[train_set]
+val_x = x[:,:,:,val_set] |> gpu
+val_y = y[val_set] |> gpu
+data = Flux.DataLoader((data=train_x, label=train_y), batchsize=batch_size) |> gpu
 
-# BSON.@save "CIFAR10_nn.bson" nn
+if retrain 
+    # CNN
+    nn = Chain(
+        Conv((5,5), 3=>16, relu),
+        MaxPool((2,2)),
+        Conv((5,5), 16=>8, relu),
+        MaxPool((2,2)),
+        x -> reshape(x, :, size(x, 4)),
+        Dense(200, 120),
+        Dense(120, 84),
+        Dense(84, 1)
+    ) |> gpu
+
+    using Flux: Momentum
+    using Flux.Losses: logitbinarycrossentropy
+    loss(x, y) = logitbinarycrossentropy(vec(nn(x)), y)
+    opt = Momentum(0.01)
+    accuracy(x, y) = mean(vec(round.(Flux.σ.(nn(x)))) .== y)
+    avg_loss(data) = mean(map(d -> loss(d[1],d[2]), data))
+
+    # Training
+    epochs = 200
+    for epoch = 1:epochs
+    for d in data
+        gs = gradient(params(nn)) do
+        l = loss(d...)
+        end
+        update!(opt, params(nn), gs)
+    end
+    println("Training loss")
+    @show avg_loss(data)
+    println("Training accuracy")
+    @show accuracy(train_x,train_y)
+    println("Test accuracy")
+    @show accuracy(val_x, val_y)
+    end
+
+    BSON.@save "CIFAR10_nn.bson" nn
+end
 
 # Load model
 BSON.@load "CIFAR10_nn.bson" nn
