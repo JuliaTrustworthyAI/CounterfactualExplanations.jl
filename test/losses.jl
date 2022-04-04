cb51e2f78d8570b90ec40295fbf94bbe20a41577
pat-alt <altmeyerpat@gmail.com>
2022-04-04 15:57:09 +0200

    removed test for hinge

--- test/losses.jl
+++ test/losses.jl
@@ -2,22 +2,6 @@ using CounterfactualExplanations
 using CounterfactualExplanations.Losses
 using Random
 
-@testset "hinge" begin
-
-    # Equality
-    t = rand([-1,1]) # target
-    a = t # logits
-    @test hinge_loss(a,t) == 0.0
-
-    # Label mapping
-    t = rand([0,1])
-    a = t==0 ? -1 : 1
-    @test hinge_loss(a,t) == 0.0
-
-    @test hinge_loss(-1,1) == 2.0
-    @test hinge_loss(100,1) == 0.0
-end
-
 @testset "Flux functions" begin
     @test !isnothing(logitbinarycrossentropy)
     @test !isnothing(logitcrossentropy)
