26e63838e0880e0da1b9ab02f3af6536d3700c63
lkeskull <L.Keskull@student.tudeflt.nl>
2023-05-21 14:21:30 +0200

    Changed R to logits

--- src/generators/gradient_based/probe.jl
+++ src/generators/gradient_based/probe.jl
@@ -17,21 +17,55 @@ function combine_losses(user_loss::Function)
     return ce::AbstractCounterfactualExplanation -> user_loss(ce) + hingeLoss(ce)
 end
 
+# function hingeLoss(ce::AbstractCounterfactualExplanation; σ²=0.01, kwargs...)
+#     f_loss = Flux.Losses.mse(
+#         logits(ce.M, CounterfactualExplanations.decode_state(ce)),
+#         ce.target_encoded;
+#         kwargs...,
+#     )
+#     # grad = ∂ℓ(ce.generator, ce.M, ce)
+#     grad = gradient(
+#         () -> Flux.Losses.mse(
+#             logits(ce.M, CounterfactualExplanations.decode_state(ce)),
+#             ce.target_encoded;
+#             kwargs...,
+#         ),
+#         Flux.params(ce.s′),
+#     )[ce.s′]
+#     gradᵀ = transpose(grad)
+
+#     identity_matrix = Matrix{Float64}(I, length(grad), length(grad))
+#     denominator = sqrt(gradᵀ * σ² * identity_matrix * grad)[1]
+
+#     normalized_gradient = f_loss / denominator
+#     ϕ = cdf(Normal(0, 1), normalized_gradient)
+#     println(normalized_gradient, "< norm > fi ", ϕ)
+#     return max(0, 1 - ϕ)
+# end
+
 function hingeLoss(ce::AbstractCounterfactualExplanation; σ²=0.01, kwargs...)
-    f_loss = Flux.Losses.logitbinarycrossentropy(
-        logits(ce.M, CounterfactualExplanations.decode_state(ce)),
-        ce.target_encoded;
-        kwargs...,
-    )
-    # grad = ∂ℓ(ce.generator, ce.M, ce)
+    # println("ce.target_encoded: ", ce.target)
+    f_loss = logits(ce.M, CounterfactualExplanations.decode_state(ce))[ce.target]
+    # println("f_loss: ", f_loss)
+    println(CounterfactualExplanations.decode_state(ce))
     grad = gradient(
-        () -> Flux.Losses.mse(
-            logits(ce.M, CounterfactualExplanations.decode_state(ce)),
-            ce.target_encoded;
-            kwargs...,
-        ),
+        () -> logits(ce.M, CounterfactualExplanations.decode_state(ce))[ce.target],
         Flux.params(ce.s′),
     )[ce.s′]
+    # f_loss = Flux.Losses.mse(
+    #     logits(ce.M, CounterfactualExplanations.decode_state(ce)),
+    #     ce.target_encoded;
+    #     kwargs...,
+    # )
+    # # grad = ∂ℓ(ce.generator, ce.M, ce)
+    # grad = gradient(
+    #     () -> Flux.Losses.mse(
+    #         logits(ce.M, CounterfactualExplanations.decode_state(ce)),
+    #         ce.target_encoded;
+    #         kwargs...,
+    #     ),
+    #     Flux.params(ce.s′),
+    # )[ce.s′]
     gradᵀ = transpose(grad)
 
     identity_matrix = Matrix{Float64}(I, length(grad), length(grad))
@@ -39,6 +73,10 @@ function hingeLoss(ce::AbstractCounterfactualExplanation; σ²=0.01, kwargs...)
 
     normalized_gradient = f_loss / denominator
     ϕ = cdf(Normal(0, 1), normalized_gradient)
-
+    # println(normalized_gradient, "< norm > fi ", ϕ)
     return max(0, 1 - ϕ)
+end
+
+function R_loss_function(ce::AbstractCounterfactualExplanation)
+    return max(0, hingeLoss(ce) - 0.5)
 end
