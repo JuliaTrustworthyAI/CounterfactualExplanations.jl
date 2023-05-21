ff72a15928a370e6c3fb254f269c4e91cd504a1a
lkeskull <L.Keskull@student.tudeflt.nl>
2023-05-21 11:38:43 +0200

    Fixed hingeloss stackoverflow

--- src/generators/gradient_based/functions.jl
+++ src/generators/gradient_based/functions.jl
@@ -13,7 +13,12 @@ function ∂ℓ(
     M::Models.AbstractDifferentiableJuliaModel,
     ce::AbstractCounterfactualExplanation,
 )
-    gs = gradient(() -> ℓ(generator, ce), Flux.params(ce.s′))[ce.s′]
+    gs = 0
+    if (ce.convergence[:converge_when] == :invalidation_rate)
+        gs = gradient(() -> ℓ(generator, ce), Flux.params(ce.s′))[ce.s′] .+ hingeLoss(ce)
+    else
+        gs = gradient(() -> ℓ(generator, ce), Flux.params(ce.s′))[ce.s′]
+    end
     return gs
 end
 
