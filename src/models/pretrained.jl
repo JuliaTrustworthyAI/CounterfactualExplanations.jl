2552213d434155b1bc3f2708531f5c46d5c68fdc
kmariuszk <kiciorm@gmail.com>
2023-05-26 10:48:03 +0200

    Update pretrained.jl with CIFAR10 functionality

--- src/models/pretrained.jl
+++ src/models/pretrained.jl
@@ -40,3 +40,22 @@ function load_fashion_mnist_vae(; strong=true)
     end
     return vae
 end
+
+function load_cifar10_mlp()
+    M = deserialize(joinpath(vision_dir, "cifar_10_mlp.jls"))
+    return M
+end
+
+function load_cifar10_ensemble()
+    M = deserialize(joinpath(vision_dir, "cifar_10_ensemble.jls.jls"))
+    return M
+end
+
+function load_cifar_10_vae(; strong=true)
+    if strong
+        vae = deserialize(joinpath(vision_dir, "cifar_10_vae_strong.jls"))
+    else
+        vae = deserialize(joinpath(vision_dir, "cifar_10_vae_weak.jls"))
+    end
+    return vae
+end
