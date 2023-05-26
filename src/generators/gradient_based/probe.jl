3168cda11247095e9a1c807eb85e9a3c120b5e1e
Vincent Pikand <pikandvincent@gmail.com>
2023-05-26 12:04:06 +0300

    Add comments and dynamic parameters

--- src/generators/gradient_based/probe.jl
+++ src/generators/gradient_based/probe.jl
@@ -2,41 +2,73 @@ import ..Objectives: losses_catalogue, distance_l1
 import LinearAlgebra: transpose, Matrix
 import Flux.Losses
 import Distributions: cdf, Normal
+"""
+    ProbeGenerator(; λ::AbstractFloat=0.1, loss::Symbol=:mse, penalty=distance_l1, kwargs...)
 
+Create a generator that generates counterfactual probes using the specified loss function and penalty function.
+
+# Arguments
+- `λ::AbstractFloat`: The regularization parameter for the generator.
+- `loss::Symbol`: The loss function to use for the generator. Defaults to `:mse`.
+- `penalty`: The penalty function to use for the generator. Defaults to `distance_l1`.
+- `kwargs`: Additional keyword arguments to pass to the `Generator` constructor.
+
+# Returns
+A `Generator` object that can be used to generate counterfactual probes.
+
+based on https://arxiv.org/abs/2203.06768
+"""
 function ProbeGenerator(;
     λ::AbstractFloat=0.1, loss::Symbol=:mse, penalty=distance_l1, kwargs...
 )
     @assert haskey(losses_catalogue, loss) "Loss function not found in catalogue."
     user_loss = losses_catalogue[loss]
-    # combined_loss = combine_losses(user_loss)
     return Generator(; loss=user_loss, penalty=penalty, λ=λ, kwargs...)
 end
-export ProbeGenerator
 
-function combine_losses(user_loss::Function)
-    return ce::AbstractCounterfactualExplanation -> user_loss(ce) + hingeLoss(ce)
-end
+"""
+    invalidation_rate(ce::AbstractCounterfactualExplanation; σ²=0.01, kwargs...)
 
-function hingeLoss(ce::AbstractCounterfactualExplanation; σ²=0.01, kwargs...)
+Calculate the invalidation rate of a counterfactual explanation.
+
+# Arguments
+- `ce::AbstractCounterfactualExplanation`: The counterfactual explanation to calculate the invalidation rate for.
+- `kwargs`: Additional keyword arguments to pass to the function.
+
+# Returns
+The invalidation rate of the counterfactual explanation.
+"""
+function invalidation_rate(ce::AbstractCounterfactualExplanation; kwargs...)
     f_loss = logits(ce.M, CounterfactualExplanations.decode_state(ce))[ce.target]
 	grad = []
-	for i in length(ce.s′)
+    # This has to be done with a for loop because flux does not know how to take a gradient from an array of logits.
+ 	for i in 1:length(ce.s′)
 		push!(grad, gradient(
 			() -> logits(ce.M, CounterfactualExplanations.decode_state(ce))[i],
 			Flux.params(ce.s′),
 		)[ce.s′])
 	end
-	println(grad)
     gradᵀ = transpose(grad)
 
     identity_matrix = Matrix{Float64}(I, length(grad), length(grad))
-    denominator = sqrt(gradᵀ * σ² * identity_matrix * grad)[1]
+    denominator = sqrt(gradᵀ * ce.params[:variance] * identity_matrix * grad)[1]
 
     normalized_gradient = f_loss / denominator
     ϕ = cdf(Normal(0, 1), normalized_gradient)
-    return max(0, 1 - ϕ)
+    return 1 - ϕ
 end
 
-function R_loss_function(ce::AbstractCounterfactualExplanation)
-    return max(0, hingeLoss(ce) - 0.5)
+"""
+    hinge_loss(ce::AbstractCounterfactualExplanation)
+
+Calculate the hinge loss of a counterfactual explanation.
+
+# Arguments
+- `ce::AbstractCounterfactualExplanation`: The counterfactual explanation to calculate the hinge loss for.
+
+# Returns
+The hinge loss of the counterfactual explanation.
+"""
+function hinge_loss(ce::AbstractCounterfactualExplanation)
+    return max(0, invalidation_rate(ce) - ce.params[:invalidation_rate])
 end
