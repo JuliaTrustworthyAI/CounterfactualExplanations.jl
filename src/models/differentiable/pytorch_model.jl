b81cf7e7ef8eca3f866f3a405404bd9b878de515
kmariuszk <kiciorm@gmail.com>
2023-05-26 16:14:29 +0200

    Implement pytorch_model.jl with PythonCall package

--- src/models/differentiable/pytorch_model.jl
+++ src/models/differentiable/pytorch_model.jl
@@ -1,21 +1,25 @@
-# ----- PyTorch Model ----- #
-using PyCall
+# Plan of implementing this:
+# 1. Translate the following code into PythonCall from PyCall
+# 2. Create a notebook where I can test this
+# 3. Update a notebook where I test this with a PyTorch model
+
+using PythonCall
 
 struct PyTorchModel <: AbstractDifferentiableModel
     nn::Any
 end
 
 function logits(M::PyTorchModel, X::AbstractArray)
-    py"""
-    import torch
-    from torch import nn
-    """
-    nn = M.nn
+    torch = pyimport("torch")
+    
     if !isa(X, Matrix)
       X = reshape(X, length(X), 1)
     end
-    ŷ = py"$nn(torch.Tensor($X).T).detach().numpy()"
+
+    ŷ = M.nn(torch.Tensor(X).T).detach().numpy()
+    
     ŷ = isa(ŷ, AbstractArray) ? ŷ : [ŷ]
+
     return ŷ'
 end
 
