d36e51a548b4fe0162fef06a62350520436092db
Rauno Arike <R.Arike@student.tudelft.nl>
2023-05-27 11:06:12 +0200

    Small fixes

--- test/models.jl
+++ test/models.jl
@@ -1,10 +1,11 @@
 using CounterfactualExplanations
 using CounterfactualExplanations.Models
+using DataFrames
 using Flux
 using LinearAlgebra
+using MLJ
 using MLUtils
 using Random
-using MLJ
 
 @testset "Models for synthetic data" begin
     for (key, value) in synthetic
@@ -30,24 +31,30 @@ end
 
 @testset "MLJ models for synthetic data" begin
     evotree = @load EvoTreeClassifier pkg = EvoTrees
-    nn = @load NeuralNetworkClassifier pkg = MLJFlux
-    xgboost = @load XGBoostClassifier pkg = XGBoost
-    mlj_models = [evotree, nn, xgboost]
+    mlj_models = [evotree]
     for (key, value) in synthetic
         name = string(key)
         @testset "$name" begin
             X = value[:data].X
+            y = value[:data].y
+            println(size(X))
+            println(size(y))
+            println("@@@@@@@@@@@")
+            println(y)
             for model in mlj_models
                 name = string(model)
-                df = DataFrame(X', :auto)
-                mach = machine(model, df, categorical(y)) |> fit!
-                M = CounterfactualExplanations.Models.MLJModel(mach)
                 @testset "$name" begin
                     @testset "Matrix of inputs" begin
+                        df = DataFrame(X', :auto)
+                        mach = machine(model(), df, categorical(y[:, 1]')) |> fit!
+                        M = CounterfactualExplanations.Models.MLJModel(mach)
                         @test size(logits(M, X))[2] == size(X, 2)
                         @test size(probs(M, X))[2] == size(X, 2)
                     end
                     @testset "Vector of inputs" begin
+                        df = DataFrame(X[:, 1]', :auto)
+                        mach = machine(model(), df, categorical(y')) |> fit!
+                        M = CounterfactualExplanations.Models.MLJModel(mach)
                         @test size(logits(M, X[:, 1]), 2) == 1
                         @test size(probs(M, X[:, 1]), 2) == 1
                     end
