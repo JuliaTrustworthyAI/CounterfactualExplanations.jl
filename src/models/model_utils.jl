e481cb3432e192e067c4e2f63c7c70938493b888
Rauno Arike <R.Arike@student.tudelft.nl>
2023-06-05 00:34:24 +0200

    Small bugfix

--- src/models/model_utils.jl
+++ src/models/model_utils.jl
@@ -22,7 +22,9 @@ function pytorch_model_loader(
 )
     sys = PythonCall.pyimport("sys")
     torch = PythonCall.pyimport("torch")
-    sys.path.append(model_path)
+    if !in(model_path, sys.path)
+        sys.path.append(model_path)
+    end
     PythonCall.pyimport(model_file => class_name)
     model = torch.load(pickle_path)
     return model
--- test/miscellaneous/neural_network_class.py
+++ test/miscellaneous/neural_network_class.py
@@ -1,16 +1,13 @@
-import os
-import torch
 from torch import nn
-from torch.utils.data import DataLoader
 
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
-        self.model = torch.nn.Sequential(
-                torch.nn.Flatten(),
-                torch.nn.Linear(2, 32),
-                torch.nn.Sigmoid(),
-                torch.nn.Linear(32, 2)
+        self.model = nn.Sequential(
+                nn.Flatten(),
+                nn.Linear(2, 32),
+                nn.Sigmoid(),
+                nn.Linear(32, 2)
             )
 
     def forward(self, x):
