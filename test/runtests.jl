42c2030d1c2d8fb4c973fc9866e37eb933daea68
Rauno Arike <R.Arike@student.tudelft.nl>
2023-06-11 20:07:02 +0200

    Small fix

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,39 +20,39 @@ synthetic = _load_synthetic()
 generators = generator_catalogue
 
 @testset "CounterfactualExplanations.jl" begin
-    # @testset "Data" begin
-    #     include("data.jl")
-    # end
+    @testset "Data" begin
+        include("data.jl")
+    end
 
-    # @testset "Data preprocessing" begin
-    #     include("data_preprocessing.jl")
-    # end
+    @testset "Data preprocessing" begin
+        include("data_preprocessing.jl")
+    end
 
-    # @testset "Generative Models" begin
-    #     include("generative_models.jl")
-    # end
+    @testset "Generative Models" begin
+        include("generative_models.jl")
+    end
 
     @testset "Counterfactuals" begin
         include("counterfactuals.jl")
     end
 
-    # @testset "Generators" begin
-    #     include("generators.jl")
-    # end
+    @testset "Generators" begin
+        include("generators.jl")
+    end
 
-    # @testset "Probe" begin
-    #     include("probe.jl")
-    # end
+    @testset "Probe" begin
+        include("probe.jl")
+    end
 
-    # @testset "Model" begin
-    #     include("models.jl")
-    # end
+    @testset "Model" begin
+        include("models.jl")
+    end
 
-    # @testset "Plotting" begin
-    #     include("plotting.jl")
-    # end
+    @testset "Plotting" begin
+        include("plotting.jl")
+    end
 
-    # @testset "Evaluation" begin
-    #     include("evaluation.jl")
-    # end
+    @testset "Evaluation" begin
+        include("evaluation.jl")
+    end
 end
