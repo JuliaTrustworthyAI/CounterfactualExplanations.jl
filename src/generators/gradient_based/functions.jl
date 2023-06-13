26b1b09ffa4859c3d02299fd01c570ab45117717
kmariuszk <kiciorm@gmail.com>
2023-06-13 18:53:06 +0200

    Implement ∂ℓ function

--- src/generators/gradient_based/functions.jl
+++ src/generators/gradient_based/functions.jl
@@ -1,6 +1,7 @@
 using Flux
 using Statistics
 using PythonCall
+using RCall
 
 """
     ∂ℓ(generator::AbstractGradientBasedGenerator, M::Models.AbstractDifferentiableModel, ce::AbstractCounterfactualExplanation)
@@ -60,6 +61,30 @@ function ∂ℓ(
     grad = PythonCall.pyconvert(Matrix, x.grad.t().detach().numpy())
 
     return grad
+end
+
+function ∂ℓ(
+    generator::AbstractGradientBasedGenerator, 
+    model::RTorchModel, 
+    ce::CounterfactualExplanation
+  ) 
+  x = ce.x
+  target = Float32.(ce.target_encoded)
+
+  model_nn = model.nn
+
+  R"""
+  x <- torch_tensor(t($x), requires_grad=TRUE)
+  target <- torch_tensor(t($target))
+  output <- $model_nn(x)
+  obj_loss <- nnf_binary_cross_entropy_with_logits(output, target)
+  obj_loss$backward()
+  """
+
+  grad = RCall.rcopy(R"t(as_array(x$grad))")
+  grad = Float32.(grad)
+
+  return grad
 end
 
 """
