59e72a38f5ecffe2fe58dd60f4b338f3cd61e366
kmariuszk <kiciorm@gmail.com>
2023-06-13 18:50:11 +0200

    Implement rtorch_file.jl

---
+++ src/models/differentiable/rtorch_file.jl
@@ -0,0 +1,27 @@
+using RCall
+
+struct RTorchModel <: AbstractDifferentiableModel
+    nn::Any
+    likelihood::Symbol
+end
+
+function logits(model::RTorchModel, x::AbstractArray)
+  if !isa(x, Matrix)
+      x = reshape(x, length(x), 1)
+  end
+
+  model_nn = model.nn
+
+  ŷ = rcopy(R"as_array($model_nn(torch_tensor(t($x))))")
+  ŷ = isa(ŷ, AbstractArray) ? ŷ : [ŷ]
+
+  return transpose(ŷ)
+end
+
+function probs(model::RTorchModel, x::AbstractArray) 
+  if model.likelihood == :classification_binary
+    return σ.(logits(model, x))
+  elseif model.likelihood == :classification_multi
+    return softmax(logits(model, x)) 
+  end
+end
