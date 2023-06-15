8569fbe71fa1e7f8f29b3638e757ed2060d7eea6
kmariuszk <kiciorm@gmail.com>
2023-06-15 11:32:11 +0200

    Rearrange preprocess_python_data

--- src/data_preprocessing/utils.jl
+++ src/data_preprocessing/utils.jl
@@ -77,39 +77,6 @@ function subsample(data::CounterfactualData, n::Int)
 end
 
 """
-    preprocess_python_data(data::CounterfactualData)
-
-Converts a `CounterfactualData` object to an input tensor and a label tensor.
-
-# Arguments
-- `data::CounterfactualData`: The data to be converted.
-
-# Returns
-- `(x_python::Py, y_python::Py)`: A tuple of tensors resulting from the conversion, `x_python` holding the features and `y_python` holding the labels.
-
-# Example
-x_python, y_python = preprocess_python_data(counterfactual_data) # converts `counterfactual_data` to tensors `x_python` and `y_python
-"""
-function preprocess_python_data(data::CounterfactualData)
-    x_julia = data.X
-    y_julia = data.y
-
-    # Convert data to tensors
-    torch = PythonCall.pyimport("torch")
-    np = PythonCall.pyimport("numpy")
-
-    x_python = Float32.(x_julia)
-    x_python = np.array(x_python)
-    x_python = torch.tensor(x_python).T
-
-    y_python = Float32.(y_julia)
-    y_python = np.array(y_python)
-    y_python = torch.tensor(y_python)
-
-    return x_python, y_python
-end
-
-"""
     preprocess_data_for_mlj(data::CounterfactualData)
     
 Helper function to preprocess `data::CounterfactualData` for MLJ models.
@@ -202,4 +169,37 @@ function select_factual(
     counterfactual_data::CounterfactualData, index::Union{Vector{Int},UnitRange{Int}}
 )
     return zip([select_factual(counterfactual_data, i) for i in index])
+end
+
+"""
+    preprocess_python_data(data::CounterfactualData)
+
+Converts a `CounterfactualData` object to an input tensor and a label tensor.
+
+# Arguments
+- `data::CounterfactualData`: The data to be converted.
+
+# Returns
+- `(x_python::Py, y_python::Py)`: A tuple of tensors resulting from the conversion, `x_python` holding the features and `y_python` holding the labels.
+
+# Example
+x_python, y_python = preprocess_python_data(counterfactual_data) # converts `counterfactual_data` to tensors `x_python` and `y_python
+"""
+function preprocess_python_data(data::CounterfactualData)
+    x_julia = data.X
+    y_julia = data.y
+
+    # Convert data to tensors
+    torch = PythonCall.pyimport("torch")
+    np = PythonCall.pyimport("numpy")
+
+    x_python = Float32.(x_julia)
+    x_python = np.array(x_python)
+    x_python = torch.tensor(x_python).T
+
+    y_python = Float32.(y_julia)
+    y_python = np.array(y_python)
+    y_python = torch.tensor(y_python)
+
+    return x_python, y_python
 end
