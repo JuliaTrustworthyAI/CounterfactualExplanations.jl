59d6432d7dcb33289a51b21cea5f667a7f4513b2
Lauri <69038889+laurikskl@users.noreply.github.com>
2023-06-16 15:07:53 +0300

    Update src/counterfactuals/core_struct.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/counterfactuals/core_struct.jl
+++ src/counterfactuals/core_struct.jl
@@ -111,7 +111,6 @@ function CounterfactualExplanation(
     adjust_shape!(ce)                                           # adjust shape to specified number of counterfactuals
     ce.s′ = encode_state(ce)            # encode the counterfactual state
     ce.s′ = initialize_state(ce)        # initialize the counterfactual state
-  
     if generator isa Generators.HeuristicBasedGenerator
         ce.search = Dict(
             :iteration_count => 0,
