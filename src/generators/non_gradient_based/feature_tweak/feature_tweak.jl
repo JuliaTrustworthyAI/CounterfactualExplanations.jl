04ba5816773fd09e8ed982a6cf1c051f514138c2
kmariuszk <kiciorm@gmail.com>
2023-06-16 17:14:34 +0200

    Redo creating generators.jl for non_gradient_based

--- src/generators/non_gradient_based/feature_tweak/feature_tweak.jl
+++ src/generators/non_gradient_based/feature_tweak/feature_tweak.jl
@@ -1,4 +1,22 @@
 """
+    FeatureTweakGenerator(; ϵ::AbstractFloat, kwargs...)
+
+Constructs a new Feature Tweak Generator object.
+
+Uses the L2-norm as the penalty to measure the distance between the counterfactual and the factual.
+According to the paper by Tolomei er al., an alternative choice here would be using the L0-norm to simply minimize the number of features that are changed through the tweak.
+
+# Arguments
+- `ϵ::AbstractFloat`: The tolerance value for the feature tweaks. Described at length in Tolomei et al. (https://arxiv.org/pdf/1706.06691.pdf).
+
+# Returns
+- `generator::HeuristicBasedGenerator`: A non-gradient-based generator that can be used to generate counterfactuals using the feature tweak method.
+"""
+function FeatureTweakGenerator(; ϵ::AbstractFloat=0.1, kwargs...)
+    return HeuristicBasedGenerator(; penalty=Objectives.distance_l2, ϵ=ϵ, kwargs...)
+end
+
+"""
     feature_tweaking(generator::FeatureTweakGenerator, ensemble::FluxEnsemble, x::AbstractArray, target::RawTargetType)
 
 Returns a counterfactual instance of `x` based on the ensemble of classifiers provided.
