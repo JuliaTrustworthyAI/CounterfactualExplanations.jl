5d2a64fc3fb0e820b30268377be526f2a7bda116
Rauno Arike <63519956+RaunoArike@users.noreply.github.com>
2023-06-17 19:08:14 +0200

    Update test/pytorch.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- test/pytorch.jl
+++ test/pytorch.jl
@@ -133,8 +133,7 @@ if VERSION >= v"1.8"
                                     max_iter=max_iter,
                                     decision_threshold=γ,
                                 )
-                                using CounterfactualExplanations:
-                                    counterfactual_probability
+                                using CounterfactualExplanations: counterfactual_probability
                                 @test !converged(counterfactual) ||
                                     target_probs(counterfactual)[1] >= γ # either not converged or threshold reached
                                 @test !converged(counterfactual) ||
