580f60cdbac4247b6a2441688742029947e5858c
Rauno Arike <R.Arike@student.tudelft.nl>
2023-06-18 14:19:20 +0200

    Fix tests

--- test/models.jl
+++ test/models.jl
@@ -8,23 +8,27 @@ using MLJDecisionTreeInterface
 using MLUtils
 using Random
 using LaplaceRedux
-using EvoTrees
 
 @testset "Standard models for synthetic data" begin
     for (key, value) in synthetic
         name = string(key)
         @testset "$name" begin
             X = value[:data].X
-            @testset "Verify the model's likelihood" begin
-                @test model[:model].likelihood == value[:data].likelihood
-            end
-            @testset "Matrix of inputs" begin
-                @test size(logits(model[:model], X))[2] == size(X, 2)
-                @test size(probs(model[:model], X))[2] == size(X, 2)
-            end
-            @testset "Vector of inputs" begin
-                @test size(logits(model[:model], X[:, 1]), 2) == 1
-                @test size(probs(model[:model], X[:, 1]), 2) == 1
+            for (likelihood, model) in value[:models]
+                name = string(likelihood)
+                @testset "$name" begin
+                    @testset "Verify correctness of likelihood field" begin
+                        @test model[:model].likelihood == value[:data].likelihood
+                    end
+                    @testset "Matrix of inputs" begin
+                        @test size(logits(model[:model], X))[2] == size(X, 2)
+                        @test size(probs(model[:model], X))[2] == size(X, 2)
+                    end
+                    @testset "Vector of inputs" begin
+                        @test size(logits(model[:model], X[:, 1]), 2) == 1
+                        @test size(probs(model[:model], X[:, 1]), 2) == 1
+                    end
+                end
             end
         end
     end
@@ -34,14 +38,14 @@ end
     for (key, value) in synthetic
         name = string(key)
         @testset "$name" begin
+            X = value[:data].X
+
             # Test the EvoTree model
             model = CounterfactualExplanations.Models.fit_model(value[:data], :EvoTree)
-            X = value[:data].X
             name = "EvoTree"
-
             @testset "$name" begin
-                @testset "Verify the model's likelihood" begin
-                    @test model[:model].likelihood == value[:data].likelihood
+                @testset "Verify correctness of likelihood field" begin
+                    @test model.likelihood == value[:data].likelihood
                 end
                 @testset "Matrix of inputs" begin
                     @test size(logits(model, X))[2] == size(X, 2)
@@ -57,8 +61,8 @@ end
             model = CounterfactualExplanations.Models.fit_model(value[:data], :DecisionTree)
             name = "DecisionTree"
             @testset "$name" begin
-                @testset "Verify the model's likelihood" begin
-                    @test model[:model].likelihood == value[:data].likelihood
+                @testset "Verify correctness of likelihood field" begin
+                    @test model.likelihood == value[:data].likelihood
                 end
                 @testset "Matrix of inputs" begin
                     @test size(logits(model, X))[2] == size(X, 2)
@@ -74,8 +78,8 @@ end
             model = CounterfactualExplanations.Models.fit_model(value[:data], :RandomForest)
             name = "RandomForest"
             @testset "$name" begin
-                @testset "Verify the model's likelihood" begin
-                    @test model[:model].likelihood == value[:data].likelihood
+                @testset "Verify correctness of likelihood field" begin
+                    @test model.likelihood == value[:data].likelihood
                 end
                 @testset "Matrix of inputs" begin
                     @test size(logits(model, X))[2] == size(X, 2)
@@ -95,40 +99,38 @@ end
                 laplace_model; likelihood=:classification_binary
             )
 
-            name = "LaplaceRedux"
-            @testset "Verify the likelihood for LaplaceRedux" begin
-                @test model.likelihood == :classification_binary
+            @testset "Verify correctness of likelihood field for LaplaceRedux" begin
+                @test model.likelihood == value[:data].likelihood
             end
         end
     end
 end
 
 @testset "Test for errors" begin
+    # test Flux models
     @test_throws ArgumentError Models.FluxModel("dummy"; likelihood=:regression)
     @test_throws ArgumentError Models.FluxEnsemble("dummy"; likelihood=:regression)
-    @test_throws ArgumentError Models.LaplaceReduxModel("dummy"; likelihood=:regression)
 
     data = CounterfactualExplanations.Data.load_linearly_separable()
     X, y = CounterfactualExplanations.DataPreprocessing.preprocess_data_for_mlj(data)
 
+    # test the EvoTree model
     M = EvoTrees.EvoTreeClassifier()
     evotree = MLJBase.machine(M, X, y)
-
     @test_throws ArgumentError Models.EvoTreeModel(evotree; likelihood=:regression)
 
+    # test the DecisionTree model
     M = MLJDecisionTreeInterface.DecisionTreeClassifier()
     tree_model = MLJBase.machine(M, X, y)
-
     @test_throws ArgumentError Models.TreeModel(tree_model; likelihood=:regression)
 
+    # test the RandomForest model
     M = MLJDecisionTreeInterface.RandomForestClassifier()
     forest_model = MLJBase.machine(M, X, y)
-
     @test_throws ArgumentError Models.TreeModel(forest_model; likelihood=:regression)
 
     M = MLJDecisionTreeInterface.DecisionTreeRegressor()
     regression_model = MLJBase.machine(M, X, y)
-
     @test_throws ArgumentError Models.TreeModel(
         regression_model; likelihood=:classification_binary
     )
@@ -136,9 +138,9 @@ end
         regression_model; likelihood=:classification_multi
     )
 
+    # test the LaplaceRedux model
     flux_model = CounterfactualExplanations.Models.fit_model(data, :Linear).model
     laplace_model = LaplaceRedux.Laplace(flux_model; likelihood=:classification)
-
     @test_throws ArgumentError Models.LaplaceReduxModel(
         laplace_model; likelihood=:classification_multi
     )
