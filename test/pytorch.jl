c3715111cbc402c1ec9e0ab0e4fab7f73af11149
Rauno Arike <R.Arike@student.tudelft.nl>
2023-06-19 20:25:48 +0200

    Modify tests

--- test/pytorch.jl
+++ test/pytorch.jl
@@ -40,7 +40,9 @@ if VERSION >= v"1.8"
                 model_pytorch = Models.PyTorchModel(model_loaded, data.likelihood)
 
                 @testset "Test for errors" begin
-                    @assert_throws ArgumentError Models.PyTorchModel(model_loaded, :regression)
+                    @test_throws ArgumentError Models.PyTorchModel(
+                        model_loaded, :regression
+                    )
                 end
 
                 @testset "$name" begin
@@ -64,101 +66,117 @@ if VERSION >= v"1.8"
         end
     end
 
-    @testset "Counterfactuals for Python models" begin
-        generator = Generators.GravitationalGenerator()
-        for (key, value) in synthetic
-            name = string(key)
-            @testset "$name" begin
-                counterfactual_data = value[:data]
-                X = counterfactual_data.X
+    for (key, generator_) in generators
+        name = uppercasefirst(string(key))
+    
+        # Feature Tweak will be tested separately
+        if generator_() isa Generators.HeuristicBasedGenerator
+            continue
+        end
+        # Python models are currently incompatible with the ClapROAR generator
+        if name == "Claproar"
+            continue
+        end
+    
+        @testset "$name" begin
+    
+            # Generator:
+            generator = deepcopy(generator_())
+    
+            for (key, value) in synthetic
+                name = string(key)
+                @testset "$name" begin
+                    counterfactual_data = value[:data]
+                    X = counterfactual_data.X
 
-                # Create and save model in the model_path directory
-                create_new_pytorch_model(counterfactual_data, model_path)
-                train_and_save_pytorch_model(
-                    counterfactual_data, model_location, pickle_path
-                )
-                model_loaded = Models.pytorch_model_loader(
-                    model_location, model_file, class_name, pickle_path
-                )
-                M = Models.PyTorchModel(model_loaded, counterfactual_data.likelihood)
+                    # Create and save model in the model_path directory
+                    create_new_pytorch_model(counterfactual_data, model_path)
+                    train_and_save_pytorch_model(
+                        counterfactual_data, model_location, pickle_path
+                    )
+                    model_loaded = Models.pytorch_model_loader(
+                        model_location, model_file, class_name, pickle_path
+                    )
+                    M = Models.PyTorchModel(model_loaded, counterfactual_data.likelihood)
 
-                # Randomly selected factual:
-                Random.seed!(123)
-                x = DataPreprocessing.select_factual(
-                    counterfactual_data, Random.rand(1:size(X, 2))
-                )
-                # Choose target:
-                y = Models.predict_label(M, counterfactual_data, x)
-                target = get_target(counterfactual_data, y[1])
-                # Single sample:
-                counterfactual = CounterfactualExplanations.generate_counterfactual(
-                    x, target, counterfactual_data, M, generator
-                )
+                    # Randomly selected factual:
+                    Random.seed!(123)
+                    x = DataPreprocessing.select_factual(
+                        counterfactual_data, Random.rand(1:size(X, 2))
+                    )
+                    # Choose target:
+                    y = Models.predict_label(M, counterfactual_data, x)
+                    target = get_target(counterfactual_data, y[1])
+                    # Single sample:
+                    counterfactual = CounterfactualExplanations.generate_counterfactual(
+                        x, target, counterfactual_data, M, generator
+                    )
 
-                @testset "Predetermined outputs" begin
-                    if generator.latent_space
-                        @test counterfactual.params[:latent_space]
+                    @testset "Predetermined outputs" begin
+                        if generator.latent_space
+                            @test counterfactual.params[:latent_space]
+                        end
+                        @test counterfactual.target == target
+                        @test counterfactual.x == x &&
+                            CounterfactualExplanations.factual(counterfactual) == x
+                        @test CounterfactualExplanations.factual_label(counterfactual) == y
+                        @test CounterfactualExplanations.factual_probability(counterfactual) ==
+                            probs(M, x)
                     end
-                    @test counterfactual.target == target
-                    @test counterfactual.x == x &&
-                        CounterfactualExplanations.factual(counterfactual) == x
-                    @test CounterfactualExplanations.factual_label(counterfactual) == y
-                    @test CounterfactualExplanations.factual_probability(counterfactual) ==
-                        probs(M, x)
+
+                    @testset "Convergence" begin
+                        @testset "Non-trivial case" begin
+                            counterfactual_data.generative_model = nothing
+                            # Threshold reached if converged:
+                            γ = 0.9
+                            max_iter = 1000
+                            counterfactual = CounterfactualExplanations.generate_counterfactual(
+                                x,
+                                target,
+                                counterfactual_data,
+                                M,
+                                generator;
+                                max_iter=max_iter,
+                                decision_threshold=γ,
+                            )
+                            using CounterfactualExplanations: counterfactual_probability
+                            @test !CounterfactualExplanations.converged(counterfactual) ||
+                                CounterfactualExplanations.target_probs(counterfactual)[1] >=
+                                γ # either not converged or threshold reached
+                            @test !CounterfactualExplanations.converged(counterfactual) ||
+                                length(path(counterfactual)) <= max_iter
+                        end
+
+                        @testset "Trivial case (already in target class)" begin
+                            counterfactual_data.generative_model = nothing
+                            # Already in target and exceeding threshold probability:
+                            y = Models.predict_label(M, counterfactual_data, x)
+                            target = y[1]
+                            γ = minimum([1 / length(counterfactual_data.y_levels), 0.5])
+                            counterfactual = CounterfactualExplanations.generate_counterfactual(
+                                x,
+                                target,
+                                counterfactual_data,
+                                M,
+                                generator;
+                                decision_threshold=γ,
+                            )
+                            @test maximum(
+                                abs.(
+                                    counterfactual.x .-
+                                    CounterfactualExplanations.decode_state(counterfactual)
+                                ),
+                            ) < init_perturbation
+                            @test CounterfactualExplanations.converged(counterfactual)
+                            @test CounterfactualExplanations.terminated(counterfactual)
+                            @test CounterfactualExplanations.total_steps(counterfactual) == 0
+                        end
+                    end
+
+                    # Clean up the temporary files
+                    remove_file(model_path)
+                    remove_file(pickle_path)
                 end
-
-                @testset "Convergence" begin
-                    @testset "Non-trivial case" begin
-                        counterfactual_data.generative_model = nothing
-                        # Threshold reached if converged:
-                        γ = 0.9
-                        max_iter = 1000
-                        counterfactual = CounterfactualExplanations.generate_counterfactual(
-                            x,
-                            target,
-                            counterfactual_data,
-                            M,
-                            generator;
-                            max_iter=max_iter,
-                            decision_threshold=γ,
-                        )
-                        using CounterfactualExplanations: counterfactual_probability
-                        @test !CounterfactualExplanations.converged(counterfactual) ||
-                            CounterfactualExplanations.target_probs(counterfactual)[1] >=
-                              γ # either not converged or threshold reached
-                        @test !CounterfactualExplanations.converged(counterfactual) ||
-                            length(path(counterfactual)) <= max_iter
-                    end
-
-                    @testset "Trivial case (already in target class)" begin
-                        counterfactual_data.generative_model = nothing
-                        # Already in target and exceeding threshold probability:
-                        y = Models.predict_label(M, counterfactual_data, x)
-                        target = y[1]
-                        γ = minimum([1 / length(counterfactual_data.y_levels), 0.5])
-                        counterfactual = CounterfactualExplanations.generate_counterfactual(
-                            x,
-                            target,
-                            counterfactual_data,
-                            M,
-                            generator;
-                            decision_threshold=γ,
-                        )
-                        @test maximum(
-                            abs.(
-                                counterfactual.x .-
-                                CounterfactualExplanations.decode_state(counterfactual)
-                            ),
-                        ) < init_perturbation
-                        @test CounterfactualExplanations.converged(counterfactual)
-                        @test CounterfactualExplanations.terminated(counterfactual)
-                        @test CounterfactualExplanations.total_steps(counterfactual) == 0
-                    end
-                end
-
-                # Clean up the temporary files
-                remove_file(model_path)
-                remove_file(pickle_path)
             end
         end
     end
--- test/runtests.jl
+++ test/runtests.jl
@@ -21,43 +21,43 @@ synthetic = _load_synthetic()
 generators = generator_catalogue
 
 @testset "CounterfactualExplanations.jl" begin
-    @testset "Data" begin
-        include("data/data.jl")
-    end
+    # @testset "Data" begin
+    #     include("data/data.jl")
+    # end
 
-    @testset "Data preprocessing" begin
-        include("data_preprocessing.jl")
-    end
+    # @testset "Data preprocessing" begin
+    #     include("data_preprocessing.jl")
+    # end
 
-    @testset "Generative Models" begin
-        include("generative_models.jl")
-    end
+    # @testset "Generative Models" begin
+    #     include("generative_models.jl")
+    # end
 
-    @testset "Counterfactuals" begin
-        include("counterfactuals.jl")
-    end
+    # @testset "Counterfactuals" begin
+    #     include("counterfactuals.jl")
+    # end
 
-    @testset "Generators" begin
-        include("generators.jl")
-    end
+    # @testset "Generators" begin
+    #     include("generators.jl")
+    # end
 
-    @testset "Probe" begin
-        include("probe.jl")
-    end
+    # @testset "Probe" begin
+    #     include("probe.jl")
+    # end
 
-    @testset "Model" begin
-        include("models.jl")
-    end
+    # @testset "Model" begin
+    #     include("models.jl")
+    # end
 
     @testset "PyTorch" begin
         include("pytorch.jl")
     end
 
-    @testset "Plotting" begin
-        include("plotting.jl")
-    end
+    # @testset "Plotting" begin
+    #     include("plotting.jl")
+    # end
 
-    @testset "Evaluation" begin
-        include("evaluation.jl")
-    end
+    # @testset "Evaluation" begin
+    #     include("evaluation.jl")
+    # end
 end
