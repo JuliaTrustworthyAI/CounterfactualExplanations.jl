82ed9cb05ba56769b193a73731e29eb86a87619a
Rauno Arike <63519956+RaunoArike@users.noreply.github.com>
2023-06-21 13:45:56 +0200

    Update test/models/pytorch.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- test/models/pytorch.jl
+++ test/models/pytorch.jl
@@ -113,15 +113,11 @@ if VERSION >= v"1.8"
                             decision_threshold=γ,
                         )
                         using CounterfactualExplanations: counterfactual_probability
-                        @test !CounterfactualExplanations.converged(
-                            counterfactual
-                        ) ||
-                            CounterfactualExplanations.target_probs(
-                            counterfactual
-                        )[1] >= γ # either not converged or threshold reached
-                        @test !CounterfactualExplanations.converged(
-                            counterfactual
-                        ) || length(path(counterfactual)) <= max_iter
+                        @test !CounterfactualExplanations.converged(counterfactual) ||
+                            CounterfactualExplanations.target_probs(counterfactual)[1] >=
+                              γ # either not converged or threshold reached
+                        @test !CounterfactualExplanations.converged(counterfactual) ||
+                            length(path(counterfactual)) <= max_iter
                     end
 
                     @testset "Trivial case (already in target class)" begin
