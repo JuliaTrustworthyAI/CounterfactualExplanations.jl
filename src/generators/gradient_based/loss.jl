469d5a03fe3bb07016bd180b4e5f34cacaaace8c
Mariusz Kicior <67654024+kmariuszk@users.noreply.github.com>
2023-06-22 11:34:43 +0200

    Update src/generators/gradient_based/loss.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/generators/gradient_based/loss.jl
+++ src/generators/gradient_based/loss.jl
@@ -59,27 +59,27 @@ function ∂ℓ(
 end
 
 function ∂ℓ(
-    generator::AbstractGradientBasedGenerator, 
-    model::Models.RTorchModel, 
-    ce::AbstractCounterfactualExplanation
-  ) 
-  x = ce.x
-  target = Float32.(ce.target_encoded)
+    generator::AbstractGradientBasedGenerator,
+    model::Models.RTorchModel,
+    ce::AbstractCounterfactualExplanation,
+)
+    x = ce.x
+    target = Float32.(ce.target_encoded)
 
-  model_nn = model.nn
+    model_nn = model.nn
 
-  R"""
-  x <- torch_tensor(t($x), requires_grad=TRUE)
-  target <- torch_tensor(t($target))
-  output <- $model_nn(x)
-  obj_loss <- nnf_binary_cross_entropy_with_logits(output, target)
-  obj_loss$backward()
-  """
+    R"""
+    x <- torch_tensor(t($x), requires_grad=TRUE)
+    target <- torch_tensor(t($target))
+    output <- $model_nn(x)
+    obj_loss <- nnf_binary_cross_entropy_with_logits(output, target)
+    obj_loss$backward()
+    """
 
-  grad = RCall.rcopy(R"t(as_array(x$grad))")
-  grad = Float32.(grad)
+    grad = RCall.rcopy(R"t(as_array(x$grad))")
+    grad = Float32.(grad)
 
-  return grad
+    return grad
 end
 
 """
