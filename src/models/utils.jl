52a96ed65069edf15617da7374748cdcb29f556d
kmariuszk <kiciorm@gmail.com>
2023-06-22 20:45:46 +0200

    Implement rtorch_model_loader

--- src/models/utils.jl
+++ src/models/utils.jl
@@ -121,6 +121,27 @@ function predict_label(M::AbstractFittedModel, counterfactual_data::Counterfactu
     return predict_label(M, counterfactual_data, X)
 end
 
-function rtorch_model_loader()
-    return error("TODO: Implement rtorch_model_loader")
+"""
+    rtorch_model_loader(model_path::String)
+
+Loads a previously saved R torch model.
+
+# Arguments
+- `model_path::String`: Path to the directory containing the model file.
+
+# Returns
+- `loaded_model`: Path to the pickle file containing the model.
+
+# Example
+```{julia}
+model = rtorch_model_loader("dev/R_call_implementation/model.pt")
+```
+"""
+function rtorch_model_loader(model_path::String)
+    R"""
+    library(torch)
+    loaded_model <- torch_load($model_path)
+    """
+
+    return R"loaded_model"
 end
