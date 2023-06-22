f0b9d8de32ccd391858b7b49a8267bd72a5c7da9
kmariuszk <kiciorm@gmail.com>
2023-06-22 18:17:17 +0200

    Fix checkstyle

--- src/models/differentiable/R/rtorch_model.jl
+++ src/models/differentiable/R/rtorch_model.jl
@@ -4,22 +4,22 @@ struct RTorchModel <: AbstractRModel
 end
 
 function logits(model::RTorchModel, x::AbstractArray)
-  if !isa(x, Matrix)
-      x = reshape(x, length(x), 1)
-  end
+    if !isa(x, Matrix)
+        x = reshape(x, length(x), 1)
+    end
 
-  model_nn = model.nn
+    model_nn = model.nn
 
-  ŷ = rcopy(R"as_array($model_nn(torch_tensor(t($x))))")
-  ŷ = isa(ŷ, AbstractArray) ? ŷ : [ŷ]
+    ŷ = rcopy(R"as_array($model_nn(torch_tensor(t($x))))")
+    ŷ = isa(ŷ, AbstractArray) ? ŷ : [ŷ]
 
-  return transpose(ŷ)
+    return transpose(ŷ)
 end
 
-function probs(model::RTorchModel, x::AbstractArray) 
-  if model.likelihood == :classification_binary
-    return σ.(logits(model, x))
-  elseif model.likelihood == :classification_multi
-    return softmax(logits(model, x)) 
-  end
+function probs(model::RTorchModel, x::AbstractArray)
+    if model.likelihood == :classification_binary
+        return σ.(logits(model, x))
+    elseif model.likelihood == :classification_multi
+        return softmax(logits(model, x))
+    end
 end
