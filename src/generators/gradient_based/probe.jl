1f4369c212f2e8bf9afdb708e551db93428daaaa
kmariuszk <kiciorm@gmail.com>
2023-06-25 20:58:43 +0200

    Probe bugfix

--- src/generators/gradient_based/probe.jl
+++ src/generators/gradient_based/probe.jl
@@ -38,7 +38,8 @@ Calculate the invalidation rate of a counterfactual explanation.
 The invalidation rate of the counterfactual explanation.
 """
 function invalidation_rate(ce::AbstractCounterfactualExplanation)
-    f_loss = logits(ce.M, CounterfactualExplanations.decode_state(ce))[ce.target]
+    index_target = findfirst(map(x -> x == ce.target, ce.data.y_levels))
+    f_loss = logits(ce.M, CounterfactualExplanations.decode_state(ce))[index_target]
     grad = []
     # This has to be done with a for loop because flux does not know how to take a gradient from an array of logits.
     for i in 1:length(ce.s′)
