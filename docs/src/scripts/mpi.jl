0f02c69729a9eb90b4386353d1e91d6f1e3b882f
pat-alt <altmeyerpat@gmail.com>
2023-08-29 13:12:25 +0200

    problem is that now using parallelize returns nothing in some case

--- docs/src/scripts/mpi.jl
+++ docs/src/scripts/mpi.jl
@@ -1,5 +1,6 @@
 using CounterfactualExplanations
 using CounterfactualExplanations.Data
+using CounterfactualExplanations.Evaluation: benchmark
 using CounterfactualExplanations.Models
 using CounterfactualExplanations.Parallelization
 import MPI
@@ -20,6 +21,6 @@ output = @with_parallelizer parallelizer begin
     generate_counterfactual(xs, target, counterfactual_data, M, generator)
 end
 
-
+bmk = benchmark(counterfactual_data; parallelizer=parallelizer)
 
 MPI.Finalize()
--- test/parallelization/mpi.jl
+++ test/parallelization/mpi.jl
@@ -3,7 +3,14 @@ using CounterfactualExplanations.Data
 using CounterfactualExplanations.Evaluation: benchmark
 using CounterfactualExplanations.Parallelization
 using Test
+
+# Initialize MPI
+import MPI
+MPI.Init()
+
 counterfactual_data = load_linearly_separable()
-parallelizer = MPIParallelizer()
+parallelizer = MPIParallelizer(MPI.COMM_WORLD)
 bmk = benchmark(counterfactual_data; parallelizer=parallelizer)
-@test true
+MPI.Finalize()
+@test MPI.Finalized()
+
