99eaa643755d4919ebaadafc1b48d3aebcd4455f
Pat Alt <55311242+pat-alt@users.noreply.github.com>
2023-08-29 12:12:36 +0200

    seems to be working now

--- src/parallelization/mpi.jl
+++ src/parallelization/mpi.jl
@@ -76,80 +76,15 @@ function CounterfactualExplanations.parallelize(
 
     MPI.Barrier(parallelizer.comm)        
 
-    println("Rank $(parallelizer.rank) done.")
+    output = MPI.gather(output, parallelizer.comm)
 
     # Collect output from all processes:
     if parallelizer.rank == 0
-        output = MPI.gather(output, parallelizer.comm)
         output = vcat(output...)
     end
 
+    MPI.Barrier(parallelizer.comm)
+
     return output
 end
 
-"""
-    @with_parallelizer(parallelizer, expr)
-
-This macro can be used to multi-process the evaluation of `f` using MPI. The function `f` should be a function that takes a single argument. The argument should be a vector of counterfactual explanations. The function will split the vector of counterfactual explanations into groups of approximately equal size and distribute them to the processes. The results are then collected and returned.
-"""
-macro with_parallelizer(parallelizer, expr)
-
-    @assert expr.head ∈ (:block, :call) "Expected a block or function call."
-    if expr.head == :block
-        expr = expr.args[end]
-    end
-
-    # Unpack arguments:
-    pllr = esc(parallelizer)
-    f = expr.args[1]
-    args = expr.args[2:end]
-
-    # Split args into positional and keyword arguments:
-    aargs = []
-    aakws = Pair{Symbol,Any}[]
-    for el in args
-        if Meta.isexpr(el, :parameters)
-            for kw in el.args
-                push!(aakws, Pair(kw.args...))
-            end
-        else
-            push!(aargs, el)
-        end
-    end
-
-    # Escape arguments:
-    escaped_args = Expr(:tuple, esc.(aargs)...)
-
-    output = quote
-        @assert CounterfactualExplanations.parallelizable($f) "`f` is not a parallelizable process."
-        collection = $escaped_args[1]
-        collection = collection |> x -> vectorize_collection(x)
-        if length($aargs) > 1
-            _args = $escaped_args[2:end]
-        end
-        
-        chunks = split_obs(collection, $pllr.n_proc)    
-        item = MPI.scatter(chunks, $pllr.comm)
-
-        if length($aargs) > 1
-            output = $f(item, _args...; $aakws...)
-        else
-            output = $f(item; $aakws...)
-        end
-
-        MPI.Barrier($pllr.comm)
-
-        println("Rank $($pllr.rank) done.")
-
-        output = MPI.gather(output, $pllr.comm)
-
-        # Collect output from all processes:
-        if $pllr.rank == 0
-            output = vcat(output...)
-            println(output)
-        end
-
-        MPI.Barrier($pllr.comm)
-    end
-    return output
-end
