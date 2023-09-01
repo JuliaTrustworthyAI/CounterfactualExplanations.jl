e8275e5cd689e4310706f5ea543e36a2c578d1fd
pat-alt <altmeyerpat@gmail.com>
2023-09-01 11:08:11 +0200

    so MPI is working as expected now with UUIDs, but realized that for some reason we get some dublicate rows

--- docs/src/scripts/mpi.jl
+++ docs/src/scripts/mpi.jl
@@ -1,11 +1,19 @@
+using MPI
+MPI.Init()
+
 using CounterfactualExplanations
 using CounterfactualExplanations.Data
 using CounterfactualExplanations.Evaluation: benchmark
 using CounterfactualExplanations.Models
 using CounterfactualExplanations.Parallelization
-using MPI: MPI
+using Logging
+using Serialization
 
-MPI.Init()
+if MPI.Comm_rank(MPI.COMM_WORLD) != 0
+    global_logger(NullLogger())
+else
+    @info "Disabling logging on non-root processes."
+end
 
 counterfactual_data = load_linearly_separable()
 M = fit_model(counterfactual_data, :Linear)
@@ -18,5 +26,7 @@ generator = GenericGenerator()
 parallelizer = MPIParallelizer(MPI.COMM_WORLD)
 
 bmk = benchmark(counterfactual_data; parallelizer=parallelizer)
+
+Serialization.serialize("docs/src/scripts/mpi_benchmark.jls", bmk)
 
 MPI.Finalize()
