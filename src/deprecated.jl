f982ed875edf263076cb31fea694b29ebe8b87b2
kmariuszk <kiciorm@gmail.com>
2023-11-27 11:50:38 +0100

    Deprecate migrated functions

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -5,3 +5,44 @@ Base.@deprecate_moved animate_path "TaijaPlotting"
 Base.@deprecate_moved voronoi "TaijaPlotting"
 
 Base.@deprecate_moved prepare_for_plotting "TaijaPlotting"
+
+Base.@deprecate_moved data_catalogue "TaijaData"
+
+Base.@deprecate_moved load_linearly_separable "TaijaData"
+
+Base.@deprecate_moved load_overlapping "TaijaData"
+
+Base.@deprecate_moved load_multi_class "TaijaData"
+
+Base.@deprecate_moved load_blobs "TaijaData"
+
+Base.@deprecate_moved load_circles "TaijaData"
+
+Base.@deprecate_moved load_moons "TaijaData"
+
+Base.@deprecate_moved load_synthetic_data "TaijaData"
+
+Base.@deprecate_moved load_california_housing "TaijaData"
+
+Base.@deprecate_moved load_credit_default "TaijaData"
+
+Base.@deprecate_moved load_gmsc "TaijaData"
+
+Base.@deprecate_moved load_german_credit "TaijaData"
+
+Base.@deprecate_moved load_uci_adult "TaijaData"
+
+Base.@deprecate_moved load_tabular_data "TaijaData"
+
+Base.@deprecate_moved load_mnist "TaijaData"
+
+Base.@deprecate_moved load_mnist_test "TaijaData"
+
+Base.@deprecate_moved load_fashion_mnist "TaijaData"
+
+Base.@deprecate_moved load_fashion_mnist_test "TaijaData"
+
+Base.@deprecate_moved load_cifar_10 "TaijaData"
+
+Base.@deprecate_moved load_cifar_10_test "TaijaData"
+
