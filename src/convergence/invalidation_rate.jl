d07cdad542fc238588addb271153b1d1f1524e81
pat-alt <altmeyerpat@gmail.com>
2023-12-07 21:25:43 +0100

    as if

--- src/convergence/invalidation_rate.jl
+++ src/convergence/invalidation_rate.jl
@@ -12,7 +12,7 @@ Checks if the counterfactual search has converged when the convergence criterion
 function converged(
     convergence::InvalidationRateConvergence, ce::AbstractCounterfactualExplanation
 )
-    ir = Generators.invalidation_rate(ce)
+    ir = Objectives.invalidation_rate(ce)
     label = Models.predict_label(ce.M, ce.data, ce.x′)[1]
     return label == ce.target && convergence.invalidation_rate > ir
 end
