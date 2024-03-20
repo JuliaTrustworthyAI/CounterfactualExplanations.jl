3a79d8ac35564ca69a499c668c0a904b45ff2c3a
pat-alt <altmeyerpat@gmail.com>
2024-03-20 15:28:30 +0100

    fixed

--- test/parallelization/parallelization.jl
+++ test/parallelization/parallelization.jl
@@ -1,6 +1,8 @@
-parallelizable(generate_counterfactual)
-parallelizable(evaluate)
-parallelizable(predict_label)
+using CounterfactualExplanations
+
+@test parallelizable(generate_counterfactual)==true
+@test parallelizable(CounterfactualExplanations.Evaluation.evaluate)==true
+@test parallelizable(predict_label)==false
 
 @testset "Threads" begin
     include("threads.jl")
