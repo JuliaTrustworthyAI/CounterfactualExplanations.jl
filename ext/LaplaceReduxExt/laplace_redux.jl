fa3055f657e7f4e17a2842e05df4a95bb84640e4
pat-alt <altmeyerpat@gmail.com>
2024-03-20 17:20:15 +0100

    this seems to be working already, just need a few more helpers for user-friendlyness

--- ext/LaplaceReduxExt/laplace_redux.jl
+++ ext/LaplaceReduxExt/laplace_redux.jl
@@ -9,21 +9,8 @@ struct LaplaceReduxModel <: Models.AbstractCustomDifferentiableModel
     model::LaplaceRedux.Laplace
     likelihood::Symbol
     function LaplaceReduxModel(model, likelihood)
-        if likelihood == :classification_binary
-            new(model, likelihood)
-        elseif likelihood == :classification_multi
-            throw(
-                ArgumentError(
-                    "`type` should be `:classification_binary`. Support for multi-class Laplace Redux is not yet implemented.",
-                ),
-            )
-        else
-            throw(
-                ArgumentError(
-                    "`type` should be in `[:classification_binary,:classification_multi]`"
-                ),
-            )
-        end
+        @assert likelihood in [:classification_binary, :classification_multi] "Likelihood should be in `[:classification_binary, :classification_multi]`"
+        return new(model, likelihood)
     end
 end
 
