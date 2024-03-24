e764e104239a32a7d83acf7e2d208d2a42a6d139
pat-alt <altmeyerpat@gmail.com>
2024-03-24 08:02:34 +0100

    coverage for encodings

---
+++ test/counterfactuals/counterfactuals.jl
@@ -0,0 +1,15 @@
+using CounterfactualExplanations
+using TaijaData
+
+# Counteractual data and model:
+counterfactual_data = CounterfactualData(load_linearly_separable()...)
+M = fit_model(counterfactual_data, :Linear)
+target = 2
+factual = 1
+chosen = rand(findall(predict_label(M, counterfactual_data) .== factual))
+x = select_factual(counterfactual_data, chosen)
+
+# Search:
+generator = GenericGenerator()
+
+include("encodings.jl")
---
+++ test/counterfactuals/encodings.jl
@@ -0,0 +1,16 @@
+@testset "encodings.jl" begin
+
+    @testset "Standardize" begin
+        dt_standardize = deepcopy(counterfactual_data)
+        dt_standardize.standardize = true
+        ce = generate_counterfactual(x, target, dt_standardize, M, generator)
+        @test typeof(ce) <: CounterfactualExplanation
+    end
+
+    @testset "Dimensionality Reduction" begin
+        generator.dim_reduction = true
+        ce = generate_counterfactual(x, target, counterfactual_data, M, generator)
+        @test typeof(ce) <: CounterfactualExplanation
+    end
+
+end
