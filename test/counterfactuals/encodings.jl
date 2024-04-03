4d726657023387244ef3ea4630d6d2487bb7353a
pat-alt <altmeyerpat@gmail.com>
2024-04-03 12:55:41 +0200

    now actually testing dim red

--- test/counterfactuals/encodings.jl
+++ test/counterfactuals/encodings.jl
@@ -1,3 +1,6 @@
+using CounterfactualExplanations.Models: load_mnist_mlp
+using MultivariateStats: MultivariateStats
+
 @testset "encodings.jl" begin
     @testset "Standardize" begin
         dt_standardize = deepcopy(counterfactual_data)
@@ -7,8 +10,15 @@
     end
 
     @testset "Dimensionality Reduction" begin
+        dt_pca = CounterfactualData(load_mnist(1000)...)
+        M = load_mnist_mlp()
+        target = 9
+        factual = 7
+        chosen = rand(findall(predict_label(M, dt_pca) .== factual))
+        x = select_factual(dt_pca, chosen)
+        dt_pca.dt = MultivariateStats.fit(MultivariateStats.PCA, dt_pca.X; maxoutdim=16)
         generator.dim_reduction = true
-        ce = generate_counterfactual(x, target, counterfactual_data, M, generator)
+        ce = generate_counterfactual(x, target, dt_pca, M, generator)
         @test typeof(ce) <: CounterfactualExplanation
     end
 end
