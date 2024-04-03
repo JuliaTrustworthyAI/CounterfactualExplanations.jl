51a9070056c4659eec52b2df6bc712f600a2eee3
pat-alt <altmeyerpat@gmail.com>
2024-04-03 13:48:42 +0200

    formatting

--- test/models/utils.jl
+++ test/models/utils.jl
@@ -1,5 +1,5 @@
 @testset "Utils" begin
-   @testset "Model Evaluation" begin
+    @testset "Model Evaluation" begin
         counterfactual_data = CounterfactualData(TaijaData.load_linearly_separable()...)
         dt_train, dt_test = CounterfactualExplanations.DataPreprocessing.train_test_split(
             counterfactual_data; test_size=0.7
@@ -7,5 +7,5 @@
         M = fit_model(dt_train, :Linear)
         CounterfactualExplanations.Models.model_evaluation(M, dt_test)
         @test true
-   end
+    end
 end
