2e6bfe5b6e57b67d297bc11d384e18c1dc502a6c
pat-alt <altmeyerpat@gmail.com>
2024-04-09 15:20:35 +0200

    hm

--- test/models/neurotree.jl
+++ test/models/neurotree.jl
@@ -7,6 +7,12 @@ using TaijaData
 m = NeuroTreeRegressor(; depth=5, nrounds=10)
 data = CounterfactualData(load_overlapping()...)
 M = fit_model(data, :NeuroTree; outsize=2, depth=4, lr=2e-2)
-predict_label(M, data)
-# mach = machine(m, X, y) 
-# MLJBase.fit!(mach)
+
+target = 2
+factual = 1
+chosen = rand(findall(predict_label(M, data) .== factual))
+x = select_factual(data, chosen)
+
+generator = GenericGenerator()
+
+
