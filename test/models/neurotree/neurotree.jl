26465e344d562c4a337ed1fb7f72153f479c1764
pat-alt <altmeyerpat@gmail.com>
2024-04-12 14:14:47 +0200

    ok, good to go I think

--- test/models/neurotree/neurotree.jl
+++ test/models/neurotree/neurotree.jl
@@ -12,20 +12,23 @@ using NeuroTreeModels
 using TaijaData
 
 @testset "NeuroTreeModel" begin
+    # Fit model to data:
     data = CounterfactualData(load_linearly_separable()...)
-    M = fit_model(data, :NeuroTree; depth=2, lr=2e-2, nrounds=50)
+    M = fit_model(data, :NeuroTree; depth=2, lr=2e-2, nrounds=50, batchsize=10)
 
+    # Select a factual instance:
     target = 2
     factual = 1
     chosen = rand(findall(predict_label(M, data) .== factual))
     x = select_factual(data, chosen)
 
-    η = 1.0
-    generator = GenericGenerator(; opt=Descent(η))
+    # Generate counterfactual explanation:
+    η = 0.1
+    generator = GenericGenerator(; opt=Descent(η), λ=0.01)
     conv = CounterfactualExplanations.Convergence.DecisionThresholdConvergence(;
-        decision_threshold=0.9, max_iter=250
+        decision_threshold=0.9, max_iter=100
     )
-    ce = generate_counterfactual(x, target, data, M, generator)
+    ce = generate_counterfactual(x, target, data, M, generator; convergence=conv)
     @test typeof(ce) <: CounterfactualExplanation
     @test CounterfactualExplanations.counterfactual_label(ce) == [target]
 end
