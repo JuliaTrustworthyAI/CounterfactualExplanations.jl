c4b792d2f43474a7b5edfde42acb7d2443751d3f
pat-alt <altmeyerpat@gmail.com>
2024-04-15 14:44:17 +0200

    done

---
+++ test/models/laplace_redux/laplace_redux.jl
@@ -0,0 +1,25 @@
+using CounterfactualExplanations
+using CounterfactualExplanations.Models
+using LaplaceRedux
+using TaijaData
+
+@testset "LaplaceRedux" begin
+    counterfactual_data =
+        TaijaData.load_linearly_separable() |>
+        x -> (Float32.(x[1]), x[2]) |> x -> CounterfactualData(x...)
+    M = Models.fit_model(counterfactual_data, :LaplaceRedux)
+
+    # Select a factual instance:
+    target = 2
+    factual = 1
+    chosen = rand(findall(predict_label(M, counterfactual_data) .== factual))
+    x = select_factual(counterfactual_data, chosen)
+
+    # Search:
+    generator = GenericGenerator()
+    ce = generate_counterfactual(x, target, counterfactual_data, M, generator)
+    @test typeof(ce) <: CounterfactualExplanation
+    @test CounterfactualExplanations.counterfactual_label(ce) == [target]
+end
+
+
