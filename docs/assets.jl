2a8656e5eda7499836f32259fb22dc261d5e0a4d
pat-alt <altmeyerpat@gmail.com>
2024-04-17 14:04:42 +0200

    trying with copies instead

--- docs/assets.jl
+++ docs/assets.jl
@@ -1,3 +1,3 @@
 # Since Documenter.jl cannot handle links to anything outside the docs/src directory, we need to copy the LICENSE and CHANGELOG.md files to the src/assets directory: https://discourse.julialang.org/t/make-documenter-jl-understand-where-to-search-local-markdown-links/84012/6?u=pat-alt
-symlink(joinpath(@__DIR__, "..", "LICENSE"), "docs/src/LICENSE")
-symlink(joinpath(@__DIR__, "..", "CHANGELOG.md"), "docs/src/CHANGELOG.md")
+cp(joinpath(@__DIR__, "..", "LICENSE"), joinpath(@__DIR__, "src/LICENSE"), force=true)
+cp(joinpath(@__DIR__, "..", "CHANGELOG.md"), joinpath(@__DIR__, "src/CHANGELOG.md"); force=true)
